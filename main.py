0cd5bfb5de14f94cea30369b0e08312503ba1259
hephtechy <41367783+hephtechy@users.noreply.github.com>
2024-07-04 20:12:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Query
+from fastapi.responses import JSONResponse
+import requests
 
 app = FastAPI()
 
+@app.get("/api/hello")
+async def hello(request: Request, visitor_name: str = Query(..., alias='visitor_name')):
+    url = "https://api.geoapify.com/v1/ipinfo?apiKey=1b12fda6b84d4f658f751913397c291f"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    response = requests.get(url)
+    message = response.json()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    client_ip = message['ip']
+    location = message["city"]["name"]
+    latitude = message['location']['latitude']
+    longitude = message['location']['longitude']
+
+    key = 'f1a9d0152acc4c86be2067de2f512b79'
+    temp_url = f'https://api.weatherbit.io/v2.0/current?lat={latitude}&lon={longitude}&key={key}'
+
+    try:
+        result = requests.get(temp_url).json()
+    except:
+        return JSONResponse(status_code=500, content={"error": "Failed to fetch temperature"})
+
+    temperature = result['data'][0]['temp']
+
+    greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
+
+    return JSONResponse({
+        "client_ip": client_ip,
+        "location": location,
+        "greeting": greeting
+    })
+
